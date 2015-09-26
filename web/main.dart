1dc65fad988303bfbf3e628a1c39ebf68f12e2c3
Davy Mitchell <daftspaniel@gmail.com>
2015-09-26 19:30:45 +0100

    First GitHub version.

---
+++ web/main.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2015, Davy Mitchell. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:simplegamelib/simplegamelib.dart';
+
+void main() {
+  Game game = new Game("My Game", '#surface');
+  Sprite player = game.createSprite("images/ninjadude.png");
+
+  player
+    ..position = new Point(0, 10)
+    ..movement = Movements.east;
+  print('starting game...');
+
+  game.start();
+}
