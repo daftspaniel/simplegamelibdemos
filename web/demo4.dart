3b21e23aaa61667600ca798160292243b31f6247
Davy Mitchell <daftspaniel@gmail.com>
2015-10-08 18:39:43 +0100

    Added demo4 and tweaked meta information.

---
+++ web/demo4.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2015, Davy Mitchell. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:simplegamelib/simplegamelib.dart';
+
+Sprite player;
+Game game = new Game("My Game", '#surface');
+
+void main() {
+  player = game.createSprite("images/ninjadude.png");
+  game.player.sprite = player;
+
+  player
+    ..position = new Point(0, 30)
+    ..movement = new Point(0, 0);
+
+  print('starting game...');
+  game.setUpKeys();
+  game.start();
+}
